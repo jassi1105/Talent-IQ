aa8374090a5851f441ba244590d57dc659661bc1
Jaswanth <jassijaswanth360@gmail.com>
2026-06-24 17:43:55 +0530

    Refactor inngest.js to improve function structure and formatting

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -6,17 +6,20 @@ import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 export const inngest = new Inngest({ id: "talent-iq" });
 
 const syncUser = inngest.createFunction(
-  { id: "sync-user" },
-  { event: "clerk/user.created" },
+  {
+    id: "sync-user",
+    triggers: [{ event: "clerk/user.created" }],
+  },
   async ({ event }) => {
     await connectDB();
 
-    const { id, email_addresses, first_name, last_name, image_url } = event.data;
+    const { id, email_addresses, first_name, last_name, image_url } =
+      event.data;
 
     const newUser = {
       clerkId: id,
       email: email_addresses[0]?.email_address,
-      name: `${first_name || ""} ${last_name || ""}`,
+      name: `${first_name || ""} ${last_name || ""}`.trim(),
       profileImage: image_url,
     };
 
@@ -31,12 +34,15 @@ const syncUser = inngest.createFunction(
 );
 
 const deleteUserFromDB = inngest.createFunction(
-  { id: "delete-user-from-db" },
-  { event: "clerk/user.deleted" },
+  {
+    id: "delete-user-from-db",
+    triggers: [{ event: "clerk/user.deleted" }],
+  },
   async ({ event }) => {
     await connectDB();
 
     const { id } = event.data;
+
     await User.deleteOne({ clerkId: id });
 
     await deleteStreamUser(id.toString());
